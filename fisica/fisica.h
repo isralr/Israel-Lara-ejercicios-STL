a78a31a94887473bce2785d442757507872a5f63
isralr <ilaramac@myuax.com>
2024-12-08 13:11:10 +0100

    Ejercicio 4 acabado

---
+++ fisica/fisica.h
@@ -0,0 +1,10 @@
+#ifndef FISICA_H
+#define FISICA_H
+
+namespace Ciencia {
+    namespace Fisica {
+        double calcularEnergia(double masa);
+    }
+}
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "matematicas/matematicas.h"
 #include "matematicas/geometria/geometria.h"
+#include "fisica/fisica.h"
 
 int main() {
     void ejecutarEjercicio1();{
@@ -19,6 +20,10 @@ int main() {
         Geometria::Circulo c(5);
         std::cout << "Area del circulo: " << c.calcularArea() << std::endl;
         std::cout << "Perimetro del circulo: " << c.calcularPerimetro() << std::endl;
+    }
+    void ejecutarEjercicio4();{
+        std::cout << "=== Ejercicio 4: Energia Relativista ===\n";
+        std::cout << "Energia: " << Ciencia::Fisica::calcularEnergia(10) << std::endl;
     }
     return 0;
 }
